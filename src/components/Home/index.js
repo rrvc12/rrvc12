ed27baab9f9e7562faa705430d0e4372730c92b0
rrvc12 <rruilova12@gmail.com>
2022-06-07 23:55:14 -0500

    Se mejora el home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,6 @@
 import './index.scss'
-import TransformText from './TransformText';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
 
 const Home  = () => {
     return (
@@ -11,21 +11,43 @@ const Home  = () => {
                     <div className='button-window'></div>
                 </div>
                 <div className="container-code">
-                    <TransformText text="const " color='#FF79C6' fontWeight="bold" display='inline'/>
-                    <TransformText text="Raúl" color='#50FA7B' fontWeight="bold" fontStyle="italic" display='inline'/>
-                    <TransformText text=" = {" display='inline'/>
-                    <br/>
-                    <TransformText text="type: " color='#50FA7B' fontStyle="italic" display='inline' top='5px'/>
-                    <TransformText text="'Full Stack Developer'" color='#E6EF86' display='inline' top='5px'/>
-                    <TransformText text="," display='inline' top='5px'/>
-                    <br/>
-                    <TransformText text="abilities: " color='#50FA7B' fontStyle="italic" display='inline' top='10px'/>
-                    <TransformText text="['HTML', 'CSS', 'Javascript', 'React', 'NodeJS']" color='#E6EF86' display='inline' top='10px'/>
+                    <div className='linecode'>
+                        <span className="code statement">const </span>
+                        <span className="code variable">Raúl</span>
+                        <span className="code"> &#61; &#123;</span>
+                    </div>
+                    <div className='linecode'>
+                        <span className='code key'>type</span>
+                        <span className="code">: '</span>
+                        <span className='code value'>Full Stack Developer</span>
+                        <span className="code">'&#44;</span>
+                    </div>
+                    <div className='linecode'>
+                        <span className='code key'>abilities</span>
+                        <span className="code">: &#91;</span>
+                        <span className='code value'>'HTML', 'CSS', 'Javascript', 'React', 'NodeJS'</span>
+                        <span className="code">&#93;</span>
+                    </div>
+                    <div className='linecode'>
+                        <span className='code key'>languages</span>
+                        <span className="code">: &#91;</span>
+                        <span className='code value'>'Spanish', 'English'</span>
+                        <span className="code">&#93;</span>
+                    </div>
+                    <div className='linecode'>
+                        <span className='code key'>age</span>
+                        <span className="code">: </span>
+                        <span className='code value number'>24</span>
+                    </div>
 
-                    <TransformText text=" }" top='45px'/>
+                    <span className='code'> &#125;</span>
                 </div>
 
             </div>
+            <div className="container-button">
+                <div className="button-run"><FontAwesomeIcon icon={faPlay} color="#bebebe"/> Run Code</div>
+            </div>
+            
 
         </div>
         </>
